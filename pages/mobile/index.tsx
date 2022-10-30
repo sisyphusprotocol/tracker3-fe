00fdf285055f03bf03735a91e4495025f433064d
ChelesteWang <1581446178@qq.com>
2022-10-30 23:10:02 +0800

    feat: mobile support

---
+++ pages/mobile/index.tsx
@@ -0,0 +1,28 @@
+import classNames from "classnames";
+import styles from "./device/index.module.css";
+
+export default function Iphone13() {
+  const url = "https://app.tracker3.io/";
+  return (
+    <div className={classNames(styles.container)}>
+      <div className={classNames(styles.device, styles["device-iphone-13"])}>
+        <div className={styles["device-frame"]}>
+          <div className={styles["device-content"]}>
+            <div style={{ width: "100%", height: 28 }}></div>
+            <iframe
+              width="100%"
+              height="100%"
+              className={styles["device-iframes-13"]}
+              src={url}
+            ></iframe>
+          </div>
+        </div>
+        <div className={styles["device-stripe"]}></div>
+        <div className={styles["device-header"]}></div>
+        <div className={styles["device-sensors"]}></div>
+        <div className={styles["device-btns"]}></div>
+        <div className={styles["device-power"]}></div>
+      </div>
+    </div>
+  );
+}
